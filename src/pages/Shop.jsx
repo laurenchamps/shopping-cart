ac19961269b710f2a0865ee62b7fc657049aef7e
Lauren Champs <laurenkchamps@gmail.com>
2024-05-22 15:04:45 +0930

    Add shop page basic layout and styling

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -5,7 +5,43 @@ export default function Shop() {
   return (
     <main className={styles.shop}>
       <PageNav />
-      <h1>Shop</h1>
+      <section className={styles.content}>
+        <h2>Products</h2>
+        <div className={styles.products}>
+          <div className={styles.card}>
+            <img src="bg.jpg" alt="" className={styles.productImage}></img>
+            <div className={styles.productDetails}>
+              <h3 className={styles.productTitle}>Item</h3>
+              <p className={styles.productDescription}>
+                This is a description of the item.
+              </p>
+              <p className={styles.productPrice}>$100.00</p>
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <img src="bg.jpg" alt="" className={styles.productImage}></img>
+            <div className={styles.productDetails}>
+              <h3 className={styles.productTitle}>Item</h3>
+              <p className={styles.productDescription}>
+                This is a description of the item
+              </p>
+              <p className={styles.productPrice}>$100.00</p>
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <img src="bg.jpg" alt="" className={styles.productImage}></img>
+            <div className={styles.productDetails}>
+              <h3 className={styles.productTitle}>Item</h3>
+              <p className={styles.productDescription}>
+                This is a description of the item
+              </p>
+              <p className={styles.productPrice}>$100.00</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
